8b618e8b5f89da72e8d722bc3de93a5b39b7eaea
jakedawkins <dawkinsjh@gmail.com>
2015-07-06 17:02:43 -0400

    finalizing of new checkouts done. added co setters. still need to fix add to gear list func.

--- public_html/PHP/Checkout.php
+++ public_html/PHP/Checkout.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../db.php');
+//require_once('../db.php');
 require_once('Gear.php');
 
 class Checkout {
@@ -9,7 +9,7 @@ class Checkout {
 	private $co_start;
 	private $co_end;
 	private $description;
-	private $gearList;
+	private $gearList = array();
 
 	//------------------------ getters ------------------------
 	public function getID() {
@@ -34,24 +34,102 @@ class Checkout {
 		return $gearList;
 	}
 
-	//------------------------  ------------------------
-	public function writeCheckout() {
-
-
-		
-		$database = new DB();
-		$sql = sprintf();
-		database->query($sql);
+	//------------------------ Setters ------------------------
+	public function setID($co_id) {
+		$this->co_id = $co_id;
+	}
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+	public function setPerson($person_id) {
+		$this->person_id = $person_id;
+	}
+	public function setStart($co_start) {
+		$this->co_start = $co_start;
+	}
+	public function setEnd($co_end) {
+		$this->co_end = $co_end;
+	}
+	public function setDescription($description) {
+		$this->description = $description;
+	}
+	public function addItemToGearList($gear_id) {
+		//if (!in_array($gear_id, $this->gearList)){
+			$this->gearList[] = $gear_id;
+		//}
 	}
 
 
+	//------------------------ DB ------------------------
+	public function retrieveCheckout($co_id){
+		$database = new DB();
+		$sql = "SELECT * FROM checkouts WHERE co_id='$co_id'";
+		$results = $database->select($sql);
+		
+		//set local vars
+		$this->co_id = $results[0]['co_id'];
+		$this->title = $results[0]['title'];
+		$this->person_id = $results[0]['person_id'];
+		$this->co_start = $results[0]['co_start'];
+		$this->co_end = $results[0]['co_end'];
+		$this->description = $results[0]['description'];
 
+		//TODO - Query DB for the checkout-gear combos
+		$sql = "SELECT gear_id FROM co_gear WHERE co_id='$co_id'";
+		$results = $database->select($sql);
 
+		//iterate through results and add to the gear array
+		foreach ($results as $row){ 
+			$this->gearList[] = $row['gear_id'];
+	 	}
+	}
 
+	public function finalizeCheckout(){
+		$database = new DB();
 
+		//if the co_id is unset, this is a new checkout
+		if(isset($this->co_id)){
+			printf("old checkout<br>");
+			// $sql = "UPDATE checkouts SET title='$title', person_id='$person_id', co_start='$co_start', co_end='$co_end', description='$description' WHERE id=$co_id";
+			// //query
+			// printf("___%s",$sql);
 
+			// $sql = "SELECT * FROM co_gear WHERE co_id='$co_id'";
+			// printf("___%s",$sql);
+			// //query
+			// //find a way to update the table
 
+		} else { //new checkout
+			//create the new checkout
+			printf("new checkout<br>");
+			$sql = "INSERT INTO checkouts(title,person_id,co_start,co_end,description) VALUES('$this->title','$this->person_id','$this->co_start','$this->co_end','$this->description')";
+			printf("___%s<br>",$sql);
+			//$database->query($sql);
 
+			//retrieve co_id from newly inserted checkout
+			$sql = "SELECT co_id FROM checkouts WHERE person_id='$this->person_id' AND co_start='$this->co_start' AND co_end='$this->co_end'";
+			printf("___%s<br>",$sql);
+			$results = $database->select($sql);
+			$this->co_id = $results[0]['co_id'];
+			printf("___id is: %d",$this->co_id);
+
+			printf("___gear list count: %d<br>",count($this->gearList));
+
+			//add the checkout gear relations
+			foreach($this->gearList as $gearItem){
+				printf("iterate...<br>");
+				//check to see if the item is available
+				if (isAvailable($gearItem,$this->co_start,$this->co_end)){
+					$sql = "INSERT INTO co_gear(gear_id,co_id) VALUES('$gearItem','$this->co_id')";	
+					printf("___%s<br>",$sql);
+					$database->query($sql);
+				} else {
+					//do some error logging here...
+					printf("_!_item not available: %s<br>",$gearItem);
+				}
+			}//foreach
+		}//else	
+	}//finalizeCheckout
 }
 
 ?>
--- public_html/test.php
+++ public_html/test.php
@@ -1,5 +1,6 @@
 <?php
 	require_once('PHP/Gear.php');
+	require_once('PHP/Checkout.php');
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +13,7 @@
 <body>
 	<?php 
 		//------------------------ basic getters ------------------------
+		/*
 		$results = getGearListWithType("Camera");
 		$count = count($results);
 		echo "<h1>count: $count</h1>";
@@ -20,16 +22,18 @@
 			printf("%s\t%s\t%s",$row['gear_id'],$row['gear_type_id'],$row['name']);
 			echo '<br />';
 	 	} 
-
+		*/
 		//------------------------ single availability ------------------------
+	 	/*
 	 	//gear id, date start, date end
 	 	if(isAvailable("1","2015-06-30 12:00:00","2015-07-01 12:00:00")){
 	 		printf("available");
 	 	} else {
 	 		printf("not available");
 	 	}
-
+		*/
 	 	//------------------------ multiple availability ------------------------
+	 	/*
 	 	echo '<br /><h2>available gear</h2><hr /><br />';
 
 	 	$results = getAvailableGear("2015-06-30 12:00:00","2015-07-01 12:00:00");
@@ -37,8 +41,9 @@
 			printf("%s\t%s\t%s",$row['gear_id'],$row['gear_type_id'],$row['name']);
 			echo '<br />';
 	 	} 
-
+		*/
 	 	//------------------------ insert ------------------------
+	 	/*
 	 	echo '<br /><h2>insert gear type</h2><hr /><br />';
 
 	 	//newGearItem("camera4-id","1");
@@ -52,6 +57,24 @@
 			echo '<br />';
 	 	} 	 	
 
-	 ?>
+		//$co = new Checkout();
+		//$co->retrieveCheckout(1);
+		*/
+	 	//------------------------ new checkout ------------------------
+	 	echo '<br /><h2>New Checkout</h2><hr /><br />';
+	 	$newCO = new Checkout();
+
+		$newCO->setTitle("jake doing stuff");
+		$newCO->setPerson(1);
+		$newCO->setStart("2015-10-15 01:00:00");
+		$newCO->setEnd("2015-10-15 03:00:00");
+		$newCO->setDescription("just wanna check out some fun stuff.");
+		$newCO->addItemToGearList(1);
+		$newCO->addItemToGearList(2);
+		$newCO->addItemToGearList(4);
+		$newCO->finalizeCheckout();
+	 
+
+		?>
 </body>
 </html>
