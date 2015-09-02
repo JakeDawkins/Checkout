aa129363fc21566f0251d7a31eba47e73b6e0050
jakedawkins <dawkinsjh@gmail.com>
2015-09-02 19:11:39 -0400

    added a template for bs-built files. Updated index and checkouts

--- public_html/checkouts.php
+++ public_html/checkouts.php
@@ -9,9 +9,9 @@
 	//require_once("models/config.php"); //needed for funcs
 	require_once('models/funcs.php'); //to fetch details about person
 
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$month = test_input($_POST['month']);
-		$year = test_input($_POST['year']);
+	if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['month'])) {
+		$month = test_input($_GET['month']);
+		$year = test_input($_GET['year']);
 
 		//calculate date from m/y
 		$date = "$year-$month-01";
@@ -32,74 +32,113 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>
-		Checkouts
-	</title>
+	<!-- INCLUDE BS HEADER INFO -->
+	<?php include('templates/bs-head.php'); ?>
+
+	<title>Checkouts</title>
 </head>
 <body>
-	<h1>Checkouts</h1>
-	<?php include('templates/nav.php'); ?>
-	<a href="new-checkout.php">New Checkout</a>
+	<!-- IMPORT NAVIGATION -->
+	<?php include('templates/bs-nav.php'); ?>
 
-	<!-- SEARCH TOOLS -->
-	<hr />
-	<h4>Search Checkouts By Date</h4>
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-		<select name="year">
-			<option value="2015" <?php if ($year == 15) echo 'selected="selected"';?>>2015</option>
-			<option value="2016" <?php if ($year == 16) echo 'selected="selected"';?>>2016</option>
-		</select>
-		<select name="month">
-			<option value="01" <?php if ($month == 1) echo 'selected="selected"';?>>January</option>
-			<option value="02" <?php if ($month == 2) echo 'selected="selected"';?>>February</option>
-			<option value="03" <?php if ($month == 3) echo 'selected="selected"';?>>March</option>
-			<option value="04" <?php if ($month == 4) echo 'selected="selected"';?>>April</option>
-			<option value="05" <?php if ($month == 5) echo 'selected="selected"';?>>May</option>
-			<option value="06" <?php if ($month == 6) echo 'selected="selected"';?>>June</option>
-			<option value="07" <?php if ($month == 7) echo 'selected="selected"';?>>July</option>
-			<option value="08" <?php if ($month == 8) echo 'selected="selected"';?>>August</option>
-			<option value="09" <?php if ($month == 9) echo 'selected="selected"';?>>September</option>
-			<option value="10" <?php if ($month == 10) echo 'selected="selected"';?>>October</option>
-			<option value="11" <?php if ($month == 11) echo 'selected="selected"';?>>November</option>
-			<option value="12" <?php if ($month == 12) echo 'selected="selected"';?>>December</option>
-		</select>
-		<input type="submit" name="submit" value="Submit" />
-	</form>
-	<hr />
+    <!-- HEADER -->
+    <div class="container-fluid gray">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>Checkouts</h1>
+                <!-- <p class="lead">A system for scheduling gear among a team</p> -->
+            </div>
+        </div><!-- /.row -->
+    </div><!-- /.container -->
 
-	<!-- RESULTS -->
-	<table style="">
-		<tr>
-			<!-- <td>co_id</td> -->
-			<td>Title</td>
-			<td>Description</td>
-			<td>Person</td>
-			<td>Start</td>
-			<td>End</td>
-			<td>Gear</td>
-		</tr>
-		<?php
-			foreach($checkouts as $checkout){
-				$person = $checkout->getPerson();
-				$personDetails = fetchUserDetails(NULL,NULL,$person);
+	<!-- <h1>Checkouts</h1> -->
 
-				printf("<tr>");
-				//printf("<td>%s</td>",$checkout->getID());
-				printf("<td><a href='checkout.php?co_id=%s'>%s</a></td>",$checkout->getID(),$checkout->getTitle());
-				printf("<td>%s</td>",$checkout->getDescription());
-				printf("<td>%s</td>",$personDetails['display_name']);
-				printf("<td>%s</td>",$checkout->getStart());
-				printf("<td>%s</td>",$checkout->getEnd());
-				printf("<td>");
-				foreach($checkout->getGearList() as $gear){
-					printf("%s<br>",getGearName($gear));
-				}
-				printf("</td></tr>");
-			}
-		?>
-	</table>
+	<br /><br />
+
+
+	<!-- CHECKOUTS TABLE CONCEPT -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- <form class="form-inline"> -->
+                <form class="form-inline" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="GET">
+                    <select class="push-bottom form-control" style="display: inline" name="year" id="sel1">
+                        <!-- <option value="2015" selected="selected">2015</option>
+                        <option value="2016">2016</option> -->
+                        <option value="2015" <?php if ($year == 15) echo 'selected="selected"';?>>2015</option>
+						<option value="2016" <?php if ($year == 16) echo 'selected="selected"';?>>2016</option>
+                    </select>
+                    <select class="push-bottom form-control" style="display: inline" name="month" id="sel2">
+						<option value="01" <?php if ($month == 1) echo 'selected="selected"';?>>January</option>
+						<option value="02" <?php if ($month == 2) echo 'selected="selected"';?>>February</option>
+						<option value="03" <?php if ($month == 3) echo 'selected="selected"';?>>March</option>
+						<option value="04" <?php if ($month == 4) echo 'selected="selected"';?>>April</option>
+						<option value="05" <?php if ($month == 5) echo 'selected="selected"';?>>May</option>
+						<option value="06" <?php if ($month == 6) echo 'selected="selected"';?>>June</option>
+						<option value="07" <?php if ($month == 7) echo 'selected="selected"';?>>July</option>
+						<option value="08" <?php if ($month == 8) echo 'selected="selected"';?>>August</option>
+						<option value="09" <?php if ($month == 9) echo 'selected="selected"';?>>September</option>
+						<option value="10" <?php if ($month == 10) echo 'selected="selected"';?>>October</option>
+						<option value="11" <?php if ($month == 11) echo 'selected="selected"';?>>November</option>
+						<option value="12" <?php if ($month == 12) echo 'selected="selected"';?>>December</option>
+                    </select>
+                    <button type="submit" name="submit" class="push-bottom full btn btn-primary">Filter</button> 
+                    <a class="btn btn-primary pull-right" href="new-checkout.php">New Checkout <span class="glyphicon glyphicon-plus"></span></a>
+                </form>
+
+            </div>
+        </div>
+
+        <br /><br />
+
+        <div class="row">
+            <div class="col-lg-12">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th class="hidden-xs">Description</th>
+                            <th>Person</th>
+                            <th>Start</th>
+                            <th>End</th>
+                            <th class="hidden-xs hidden-sm">Gear</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+						<?php
+							foreach($checkouts as $checkout){
+								$person = $checkout->getPerson();
+								$personDetails = fetchUserDetails(NULL,NULL,$person);
+
+								printf("<tr>");
+								printf("<td><a href='checkout.php?co_id=%s'>%s</a></td>",$checkout->getID(),$checkout->getTitle());
+								printf("<td class=\"hidden-sm\">%s</td>",$checkout->getDescription());
+								printf("<td>%s</td>",$personDetails['display_name']);
+								printf("<td>%s</td>",$checkout->getStart());
+								printf("<td>%s</td>",$checkout->getEnd());
+								printf("<td class=\"hidden-xs hidden-sm\">");
+								foreach($checkout->getGearList() as $gear){
+									printf("%s<br>",getGearName($gear));
+								}
+								printf("</td></tr>");
+							}
+						?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div> <!-- /container -->
+
+    <!-- INCLUDE BS STICKY FOOTER -->
+    <?php include('templates/bs-footer.php'); ?>
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
 
 </body>
 </html>
--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,8 @@
 <?php
+	//USER CAKE
+	require_once("models/config.php");
+	if (!securePage($_SERVER['PHP_SELF'])){die();}
+
 	//require_once('model/db.php');
 	require_once('models/Gear.php');
 	require_once('models/Checkout.php');
@@ -9,54 +13,14 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="checkout system">
-    <meta name="author" content="Jake Dawkins">
+	<!-- INCLUDE BS HEADER INFO -->
+	<?php include('templates/bs-head.php'); ?>
 
     <title>Welcome!</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/mainstyle.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
-    <!-- NAVIGATION -->
-    <!-- navbar-inverse --> 
-    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Checkout</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="inventory.php">Inventory</a></li>
-                    <li><a href="checkouts.php">Checkouts</a></li>
-                    <li><a href="account.php">Account</a></li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+	<!-- IMPORT NAVIGATION -->
+	<?php include('templates/bs-nav.php'); ?>
 
     <!-- HEADER -->
     <div class="container-fluid gray">
@@ -82,17 +46,13 @@
 
     </div> <!-- /container -->
 
-    <footer class="footer">
-      <div class="container text-center">
-        <p class="text-muted">Place sticky footer content here.</p>
-      </div>
-    </footer>
+    <!-- INCLUDE BS STICKY FOOTER -->
+    <?php include('templates/bs-footer.php'); ?>
 
     <!-- jQuery Version 1.11.1 -->
     <script src="js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-    <!-- <?php include('templates/nav.php'); ?> -->
 </body>
 </html>
