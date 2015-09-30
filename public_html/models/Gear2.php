d88d717f8f372cecc6f5a1c964caa5ebce84ca4d
jakedawkins <dawkinsjh@gmail.com>
2015-09-30 11:34:26 -0400

    gear2 is in progress. Not sure if going to use later. commiting for archive sake

--- public_html/models/Gear2.php
+++ public_html/models/Gear2.php
@@ -8,6 +8,7 @@ class Gear {
 	private $qty;
 
 	//------------------------ static methods ------------------------
+	//doesn't return a list of gear objects
 	public static function getGearList(){
 		return getGearListWithType(NULL);
 	}
@@ -21,12 +22,42 @@ class Gear {
 		return $database->select($sql);
 	}
 
+	//doesn't return a list of gear objects
 	public static function getAvailableGear($co_start, $co_end){
 		return getAvailableGearWithType(NULL, $co_start, $co_end);
 	}
 
 	public static function getAvailableGearWithType($type, $co_start, $co_end){
-		
+		//$return_gear = array();
+		$database = new DB();
+
+		if (is_null($type)){
+			$sql = 	"SELECT * FROM gear";
+		} elseif (!is_numeric($type)) { //type name passed in
+			//get id of type
+			$sql = "SELECT gear_type_id FROM gear_types WHERE type='$type'";
+			$results = $database->select($sql);
+			$type_id = $results[0]['gear_type_id'];//$row["gear_type_id"];
+
+			$sql = "SELECT * FROM gear WHERE gear_type_id='$type_id' ORDER BY name";
+		} else { //type ID passed in
+			$sql = "SELECT * FROM gear WHERE gear_type_id='$type' ORDER BY name";
+		}
+
+		$results = $database->select($sql);
+
+		$available_gear = array();
+
+		foreach($results as $row) {
+			//printf("__GEAR_ID___:%d",$row['gear_id']);
+			//echo '<br>';
+    		if(isAvailable($row['gear_id'],$co_start, $co_end)){
+        		//add object to return array
+        		$available_gear[] = $row;
+    		}
+		}
+
+		return $available_gear;
 	}
 
 	//------------------------ getters ------------------------
@@ -97,6 +128,20 @@ class Gear {
 
 		}
 	}//end finalize
+
+	//lists the available quantity of any item. 
+	public function availableQty($gear_id, $co_start, $co_end){
+		$database = new DB();
+
+		//list rentals in time range
+		$sql = "SELECT * FROM checkouts INNER JOIN co_gear ON checkouts.co_id = co_gear.co_id WHERE co_start < '$co_end' AND co_end > '$co_start'";
+
+		$results = $database->select($sql);
+
+		//TODO
+
+	}
+
 
 
 }//end class
