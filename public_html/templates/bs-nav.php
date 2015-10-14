dd924ff2fd85ace825a58fbf93453b32a2d5e1ce
jakedawkins <dawkinsjh@gmail.com>
2015-10-14 16:04:28 -0400

    changed logo to glyphicons check

--- public_html/templates/bs-nav.php
+++ public_html/templates/bs-nav.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="checkouts.php">Checkout</a>
+                <a class="navbar-brand" href="checkouts.php"><span class="glyphicon glyphicon-check"></span></a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
