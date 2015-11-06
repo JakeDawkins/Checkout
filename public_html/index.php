fa1475ddc5ca39880203e46701317d9d088653ff
jakedawkins <dawkinsjh@gmail.com>
2015-11-06 10:17:33 -0500

    fixed accidental removal of funcs.php and added cal file to start building a calendar view for checkouts

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@ if (!securePage(htmlspecialchars($_SERVER['PHP_SELF']))){die();}
 
 	require_once('models/Gear.php');
 	require_once('models/Checkout.php');
-    require_once('models/funcs.php')
+    require_once('models/funcs.php');
 ?>
 
 <!DOCTYPE html>
