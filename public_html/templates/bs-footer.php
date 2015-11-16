402808087876dbde358e39085b72e1f8ae0fd9ad
jakedawkins <dawkinsjh@gmail.com>
2015-11-16 09:01:28 -0500

    fixed typo in footer preventing redirect to feeback

--- public_html/templates/bs-footer.php
+++ public_html/templates/bs-footer.php
@@ -1,6 +1,6 @@
     <footer class="footer color-sec" style="padding-top: 15px;">
       <div class="container text-center">
         <p>&copy;2015 <a href="http://jakedawkins.com">Jake Dawkins</a>&nbsp;&nbsp;|
-        &nbsp;<a href"feedback.php">Feedback</a></p>
+        &nbsp;<a href="feedback.php">Feedback</a></p>
       </div>
     </footer>
